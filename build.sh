20a09eee0f769d2bf8288d5dadd5fa7bd8f2f060
Nick Bargnesi <nbargnesi@selventa.com>
2012-08-13 14:41:23 -0400

    General Python and C code in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -x verbose
 java -cp antlr-3.4.jar org.antlr.Tool BELScript_v1.g || exit 1
+java -cp antlr-3.4.jar org.antlr.Tool BELScript_C_v1.g || exit 1
+java -cp antlr-3.4.jar org.antlr.Tool BELScript_Python_v1.g || exit 1
 javac -cp antlr-3.4.jar *.java || exit 1
 
